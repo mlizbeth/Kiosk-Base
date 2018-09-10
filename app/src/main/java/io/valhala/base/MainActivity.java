fbbe719ba8eb4862fcdac1a0469d79bdfe85ad45
mlizbeth <mirgantrophy@gmail.com>
2018-09-10 15:33:34 -0500

    update for USB scanner support

--- app/src/main/java/io/valhala/base/MainActivity.java
+++ app/src/main/java/io/valhala/base/MainActivity.java
@@ -13,7 +13,14 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.InputType;
+import android.text.TextWatcher;
+import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -30,9 +37,12 @@ import com.google.api.services.sheets.v4.model.AppendValuesResponse;
 import com.google.api.services.sheets.v4.model.ValueRange;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -42,21 +52,15 @@ public class MainActivity extends AppCompatActivity {
     private NetHttpTransport HTTP_TRANSPORT;
     private GoogleAccountCredential credential;
     private GoogleSignInAccount signInAccount;
-    private static final int frequency = 44100;
-    private static final int channelConfig = AudioFormat.CHANNEL_CONFIGURATION_MONO;
-    private static final int audioEncoding = AudioFormat.ENCODING_PCM_16BIT;
-    private int bufferSize;
-    private AudioRecord audioRecord;
     private AsyncTask<String, Void, Boolean> writeTask;
-    private AsyncTask<Void, Void, ParseResult> task;
     private static final int REQUEST_CODE_ASK_PERM = 1;
     private static final int REQUEST_CODE_SIGN_IN = 0;
-    private static final String[] REQUIRED_PERMISSION = new String[] {Manifest.permission.RECORD_AUDIO,Manifest.permission.INTERNET, Manifest.permission.GET_ACCOUNTS};
+    private static final String[] REQUIRED_PERMISSION = new String[] {Manifest.permission.INTERNET, Manifest.permission.GET_ACCOUNTS, Manifest.permission.WRITE_EXTERNAL_STORAGE};
     private static final String APPLICATION_NAME = "";
     private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
     private Sheets service;
     private Student student;
-    private Button submitBtn;
+    private EditText idField;
     private String reason;
     private boolean valid;
 
@@ -66,10 +70,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         checkPermissions();
 
-        try {
-            bufferSize = AudioRecord.getMinBufferSize(frequency, channelConfig, audioEncoding) * 8;
-            audioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC, frequency, channelConfig, audioEncoding, bufferSize);
-        } catch(Exception e) {}
 
         if(account == null) {new Login().execute();}
         setContentView(R.layout.activity_main);
@@ -79,58 +79,39 @@ public class MainActivity extends AppCompatActivity {
 
     private void init() {
         student = new Student();
-    }
+        idField = findViewById(R.id.editText);
+        idField.setInputType(InputType.TYPE_NULL);
+        idField.requestFocus();
+        idField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) { }
 
-    private void verifyID(String data) {
-        barcode = "";
-        String[] delimiter = {";", "?"};
-        int min_length = 9, max_length = 11;
-        if(data.length() == min_length) {
-            barcode = data.substring(data.indexOf(delimiter[0] + 1), data.indexOf(delimiter[1]) - 1);
-            student.setId(barcode);
-            onValidId();
-        }
-        else if(data.length() == max_length) {
-            barcode = data.substring((data.indexOf(delimiter[0]) + 1), data.indexOf(delimiter[1]) - 2);
-            student.setId(barcode);
-            onValidId();
-        }
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(idField.getText().length() == 11) {
+                    verifyID(idField.getText().toString());
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) { }
+        });
     }
 
     private void onValidId() {
         valid = true;
         reason = "";
-        submitBtn.setOnClickListener(e -> {
-            submitBtn.setEnabled(false);
-            student.setReason(reason);
-            student.setTimeStamp();
-            writeTask = new Write().execute();
-        });
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        audioRecord.startRecording();
-        task = new MonitorAudioTask();
-        task.execute(null, null, null);
+        idField.requestFocus();
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        task.cancel(true);
-        audioRecord.stop();
-    }
-
-    private void restart() {
-        Intent intent = getIntent();
-        overridePendingTransition(0, 0);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-        finish();
-        overridePendingTransition(0, 0);
-        intent.putExtra("Account", account);
-        startActivity(intent);
     }
 
     protected void checkPermissions() {
@@ -165,125 +146,22 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private class MonitorAudioTask extends AsyncTask<Void, Void, ParseResult> {
-        @Override
-        protected ParseResult doInBackground(Void... params) {
-            final double QUIET_THRESHOLD = 32768.0 * 0.02; //anything higher than 0.02% is considered non-silence
-            final double QUIET_WAIT_TIME_SAMPLES = frequency * 0.25; //~0.25 seconds of quiet time before parsing
-            short[] buffer = new short[bufferSize];
-            Long bufferReadResult = null;
-            boolean nonSilence = false;
-            ParseResult result = null;
+    private void verifyID(String data) {
+        barcode = "";
+        String[] delimiter = {";", "?"};
+        barcode = data.substring((data.indexOf(delimiter[0]) + 1), data.indexOf(delimiter[1]) - 2);
+        student.setId(barcode);
+        onValidId();
+    }
 
-            if(valid) {task.cancel(true);}
-
-            while (!nonSilence) {
-                if (isCancelled()) {break;}
-                bufferReadResult = new Long(audioRecord.read(buffer, 0, bufferSize));
-                if (bufferReadResult > 0) {
-                    for (int i = 0; i < bufferReadResult; i++)
-                        if (buffer[i] >= QUIET_THRESHOLD) {
-                            ByteArrayOutputStream stream = new ByteArrayOutputStream();
-                            long silentSamples = 0;
-
-                            //Save this data so far
-                            for (int j = i; j < bufferReadResult; j++) {
-                                stream.write(buffer[j] & 0xFF);
-                                stream.write(buffer[j] >> 8);
-                            }
-
-                            //Keep reading until we've reached a certain amount of silence
-                            boolean continueLoop = true;
-                            while (continueLoop) {
-                                bufferReadResult = new Long(audioRecord.read(buffer, 0, bufferSize));
-                                if (bufferReadResult < 0)
-                                    continueLoop = false;
-
-                                for (int k = 0; k < bufferReadResult; k++) {
-                                    stream.write(buffer[k] & 0xFF);
-                                    stream.write(buffer[k] >> 8);
-                                    if (buffer[k] >= QUIET_THRESHOLD || buffer[k] <= -QUIET_THRESHOLD)
-                                        silentSamples = 0;
-                                    else
-                                        silentSamples++;
-                                }
-
-                                if (silentSamples >= QUIET_WAIT_TIME_SAMPLES)
-                                    continueLoop = false;
-                            }
-
-                            //Convert to array of 16-bit shorts
-                            byte[] array = stream.toByteArray();
-                            short[] samples = new short[array.length / 2];
-                            for (int k = 0; k < samples.length; k++)
-                                samples[k] = (short) ((short) (array[k * 2 + 0] & 0xFF) | (short) (array[k * 2 + 1] << 8));
-
-                            //Try parsing the data now!
-                            result = CardDataParser.Parse(samples);
-                            if (result.errorCode != 0) {
-                                //Reverse the array and try again (maybe it was swiped backwards)
-                                for (int k = 0; k < samples.length / 2; k++) {
-                                    short temp = samples[k];
-                                    samples[k] = samples[samples.length - k - 1];
-                                    samples[samples.length - k - 1] = temp;
-                                }
-                                result = CardDataParser.Parse(samples);
-                            }
-
-                            nonSilence = true;
-                            break;
-                        }
-                } else
-                    break;
-            }
-
-            return result;
-        }
-
-
-        protected void onPostExecute(ParseResult result) {
-            if (result != null) {
-                String str = result.data;
-
-                if (result.errorCode == 0) { } //why is this here maddie¿
-                else {
-                    String err = Integer.toString(result.errorCode);
-                    switch (result.errorCode) {
-                        case -1: {
-                            err = "NOT_ENOUGH_PEAKS";
-                            break;
-                        }
-                        case -2: {
-                            err = "START_SENTINEL_NOT_FOUND";
-                            break;
-                        }
-                        case -3: {
-                            err = "PARITY_BIT_CHECK_FAILED";
-                            break;
-                        }
-                        case -4: {
-                            err = "LRC_PARITY_BIT_CHECK_FAILED";
-                            break;
-                        }
-                        case -5: {
-                            err = "LRC_INVALID";
-                            break;
-                        }
-                        case -6: {
-                            err = "NOT_ENOUGH_DATA_FOR_LRC_CHECK";
-                            break;
-                        }
-                    }
-
-                    str += err;
-                }
-                verifyID(str);
-            }
-            task = null;
-            task = new MonitorAudioTask();
-
-            task.execute(null, null, null);
-        }
+    private void restart() {
+        Intent intent = getIntent();
+        overridePendingTransition(0, 0);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+        finish();
+        overridePendingTransition(0, 0);
+        intent.putExtra("Account", account);
+        startActivity(intent);
     }
 
     private class Write extends AsyncTask<String, Void, Boolean> {
@@ -343,7 +221,7 @@ public class MainActivity extends AppCompatActivity {
                 );
 
                 credential.setSelectedAccount(account);
-                credential.setSelectedAccountName("");
+                credential.setSelectedAccountName("tigerlearningcommons@trinity.edu");
 
                 Sheets service = new Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential)
                         .setApplicationName(APPLICATION_NAME)
